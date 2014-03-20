a118e2ffed796ae463f373f87c4a0d71924e7609
Micah <micah@whamdonk.com>
2014-03-20 13:31:28 -0500

    Style upgrades + a showHome call

--- public/js/src/router.js
+++ public/js/src/router.js
@@ -1,14 +1,18 @@
 define(['backbone'], function (Backbone) {
+    "use strict";
     return Backbone.Router.extend({
         app : null,
-        initialize : function () {
-            this.app = new Backbone.View;
+        initialize : function (params) {
+            params = params || {};
+            this.app = params.app || new Backbone.View();
         },
-        routes:{
+        routes: {
             '' : 'home'
         },
         home : function () {
-        
+            if (this.app.showHome) {
+                this.app.showHome();
+            }
         }
         
     });
