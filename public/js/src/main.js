2ee70170248dada7436ce362c6f0c5f54d3ac3e3
Micah <micah@whamdonk.com>
2014-03-20 13:30:36 -0500

    Cleaned up the formatting a little bit

--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,19 +1,15 @@
 require.config({ 
-     baseUrl:'/js/src',
-       paths: { 
-           jquery:'../lib/jquery/dist/jquery.min', 
-           underscore:'../lib/underscore/underscore', 
-           backbone:'../lib/backbone/backbone', 
-      bootstrap:'../lib/bootstrap/dist/js/bootstrap' 
+    baseUrl: '/js/src',
+    paths: { 
+        jquery:     '../lib/jquery/dist/jquery.min', 
+        underscore: '../lib/underscore/underscore', 
+        backbone:   '../lib/backbone/backbone', 
+        bootstrap:  '../lib/bootstrap/dist/js/bootstrap' 
     }, 
     shim: { 
-        bootstrap:{ 
-            deps:['jquery'], 
-            exports:'$.fn.popover' 
+        bootstrap: { 
+            deps:    ['jquery'], 
+            exports: '$.fn.popover' 
         } 
     } 
-});  
-                        
-define(['router'], function (Router) {
-    var r = new Router();
 });
