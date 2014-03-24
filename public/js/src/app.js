6060ab8bbcac4ca0129f94478b00a7ecf9bac253
Micah <micah@whamdonk.com>
2014-03-24 10:40:34 -0500

    Pass a reference to App into the router class

--- public/js/src/app.js
+++ public/js/src/app.js
@@ -1,9 +1,12 @@
 define(['underscore', 'backbone', 'views/layout', 'router'], function (_, Backbone, LayoutView, Router) {
     'use strict';
 
-    return {
-        pub_sub : _.extend(Backbone.Events),
-        layout:    new LayoutView(),
-        router:    new Router()
-    };
+    var app = {
+            pub_sub : _.extend(Backbone.Events),
+            layout:    new LayoutView()
+        };
+    // Add the router with a pointer back to itself
+    app.router = new Router({app: app});
+
+    return app;
 });
