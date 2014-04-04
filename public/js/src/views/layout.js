b9861fa3a0ec9f0dbe8058f64bfe7368d68783a4
Micah <micah@whamdonk.com>
2014-04-04 13:18:28 -0500

    Implement the navbar

--- public/js/src/views/layout.js
+++ public/js/src/views/layout.js
@@ -1,7 +1,17 @@
-define(['backbone'], function (Backbone) {
+define(['backbone', 'modules/navigation/views/navigation', 'modules/navigation/collections/nav_links'], function (Backbone, NavigationView, NavCollection) {
     'use strict';
     return Backbone.View.extend({
-        sidebar:null,
-        content:null
+        navbar: null,
+        content: null,
+        initialize : function () {
+            this.navbar = new NavigationView({el:$('nav').eq(0),collection: new NavCollection([{href:'#',label:'Link1'},{href:'#',label:'Link number dos'},{href:'#',label:'And ANOTHER thing'},{href:'#',label:'Blah', active:true}])});
+
+            this.render();
+        },
+        render : function () {
+            this.navbar.render();
+//            this.$el.html(this.navbar.render().$el);
+            return this;
+        }
     });
 });
